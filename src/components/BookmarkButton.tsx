5fa57413de59c684e7ac6c531f20e64835415ca1
Colina Lozito <luisdicoloze@gmail.com>
2026-03-21 01:01:15 +0100

    bookmark button interface refactor

--- src/components/BookmarkButton.tsx
+++ src/components/BookmarkButton.tsx
@@ -1,34 +1,13 @@
 import { Bookmark, BookmarkCheck } from '@tamagui/lucide-icons'
-import { Button, GetThemeValueForKey, useTheme } from 'tamagui'
+import { Button, ButtonProps, GetThemeValueForKey, useTheme } from 'tamagui'
 
-interface BookmarkButtonProps {
+interface BookmarkButtonProps extends ButtonProps {
   isBookmarked: boolean
-  onPress: () => void
-  size?: number
-  scaleIcon?: number
-  backgroundColor?: string
-  opacity?: number
-  position?: 'absolute' | 'relative'
-  right?: number
-  bottom?: number
-  left?: number
-  top?: number
-  disabled?: boolean
 }
 
 export default function BookmarkButton({
   isBookmarked,
-  onPress,
-  size = 68,
-  scaleIcon = 1,
-  backgroundColor,
-  opacity = 1,
-  position,
-  right,
-  bottom,
-  left,
-  top,
-  disabled = false,
+  ...buttonProps
 }: BookmarkButtonProps) {
   const theme = useTheme()
 
@@ -38,28 +17,14 @@ export default function BookmarkButton({
 
   return (
     <Button
-      size={size}
       circular
       elevate
       icon={isBookmarked ? BookmarkCheck : Bookmark}
-      onPress={onPress}
-      disabled={disabled}
       color={theme.text.val}
-      scaleIcon={scaleIcon}
       shadowColor={shadowColor}
       shadowOpacity={0.3}
       shadowRadius={8}
-      opacity={opacity}
-      position={position}
-      right={right}
-      bottom={bottom}
-      left={left}
-      top={top}
-      style={
-        backgroundColor
-          ? { backgroundColor }
-          : undefined
-      }
+      {...buttonProps}
     />
   )
 }

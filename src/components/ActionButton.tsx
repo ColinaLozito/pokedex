e63ea619e6aa407135fb92072ee1dcd392fb7030
Colina Lozito <luisdicoloze@gmail.com>
2026-03-21 01:02:06 +0100

    action button to design system

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -1,5 +1,4 @@
-import { baseColors } from '@theme/colors'
-import { Button, Text, useTheme } from 'tamagui'
+import { Button, Text } from 'tamagui'
 
 interface ActionButtonProps {
   text: string
@@ -7,19 +6,18 @@ interface ActionButtonProps {
 }
 
 export function ActionButton({ text, onPress }: ActionButtonProps) {
-  const theme = useTheme()
   
   return (
     <Button
       onPress={onPress}
-      borderWidth={2}
-      borderColor={theme.red?.val || baseColors.red}
-      backgroundColor={"transparent"}
+      borderWidth="$1"
+      borderColor="$red"
       width="45%"
-      height={70}
+      height="$6"
+      backgroundColor="$white"
       pressStyle={{ scale: 0.95 }}
     >
-      <Text color={theme.red?.val || baseColors.red} fontSize={18} fontWeight={500}>
+      <Text color="$red" fontSize="$3" fontWeight="$5">
         {text}
       </Text>
     </Button>

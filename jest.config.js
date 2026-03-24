bfb2e32a8f590e96afcfd44981c70c62e1d4daea
Colina Lozito <luisdicoloze@gmail.com>
2026-03-24 20:26:09 +0100

    coverage activated

--- jest.config.js
+++ jest.config.js
@@ -11,4 +11,19 @@ module.exports = {
     'node_modules/(?!(react-native|@react-native|expo|@expo|@unimodules|unimodules|expo-modules-core)/)',
   ],
   testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+    '!src/**/types/**',
+    '!src/**/constants/**',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 70,
+      branches: 70,
+      functions: 70,
+      lines: 70,
+    },
+  },
 }
